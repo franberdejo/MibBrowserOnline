41585cc75ed5f1d868292ec97209c4943bc0cd1a
= <=>
2021-05-08 19:02:46 +0200

    scaner de ip completado

--- src/routes/ips.js
+++ src/routes/ips.js
@@ -5,13 +5,28 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const snmp = require ("net-snmp");
-var portscanner = require('portscanner');
 
 //Puerto por defecto de los agentes SNMP, el único que comprobaremos de momento
 const snmpport=161;
 
-//Las variables en las que guardaremos los datos recibidos en la petición AJAX
-var ip,mask;
+//agentes con snmp activo tras el scan
+var agents=[];
+
+//Funcion que espera al SCAN para devolver la respuesta
+function espera (res){
+    res.send(agents);
+}
+
+//Funcion que comprueba si hay agente snmp enviandole una peticion
+function sonda (equipo){
+    session = snmp.createSession(equipo, 'public');
+    session.get (['1.3.6.1.2.1.1.3.0'], function(error, varbinds){
+        if(error){
+        }else{
+            agents.push(equipo)
+        }
+    });
+}
 
 //Funcion que responde a la peticion http AJAX
 function respuesta(req, res){
@@ -19,23 +34,17 @@ function respuesta(req, res){
     subnet= req.query.ip;
     console.log("Escaneando equipos en la misma subred que "+subnet+'/'+24);
 
-    var agents=[];
     var ip = req.query.ip.match(/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/);
-    
-    /*IMPORTANTE, AHORA MISMO SÓLO FUNCIONA PARA REDES DE MÁSCARA 24*/
+    agents = []
+
+    //IMPORTANTE, AHORA MISMO SÓLO FUNCIONA PARA REDES DE MÁSCARA 24
     for(let i=1;i<255;i++){
         //IP de cada equipo de la subred
         var equipo = [(ip[1]),(ip[2]),(ip[3]),i].join('.');
-
-        setTimeout(function(){portscanner.checkPortStatus(snmpport, equipo, function(error, status) {
-            console.log(equipo + ' ' + status);
-            agents.push(equipo);
-            })}
-        ,200);
+        sonda(equipo)
     }
-    //Devuelve un array con los equipos de la subred que tienen agente SNMP
-    res.send(agents);
-} 
+    setTimeout(espera.bind(null, res), 1000);
+}
 
 //A la escucha en la url /peticion
 router.get('/ips', respuesta)
