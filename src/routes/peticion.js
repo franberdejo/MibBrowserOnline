7b7402cf551986dc36d3dd20a8264f5ac5a492d9
Javier Esteban <43547137+jesteban98@users.noreply.github.com>
2021-05-12 19:54:59 +0200

    Añadido getTable
    Funciona, pero debe mostrarse aún por pantalla

--- src/routes/peticion.js
+++ src/routes/peticion.js
@@ -16,8 +16,48 @@ const comunidad = 'public';
 //Las variables en las que guardaremos los datos recibidos en la petición AJAX
 var ip,operacion,mib,oid;
 //Respuesta del AJAX
-var resultadoConsulta
+var resultadoConsulta;
+function sortInt (a, b) {
+    if (a > b)
+        return 1;
+    else if (b > a)
+        return -1;
+    else
+        return 0;
+}
 
+function callbackTable (error, table) {
+    if (error) {
+        console.error (error.toString ());
+    } else {
+        // This code is purely used to print rows out in index order,
+        // ifIndex's are integers so we'll sort them numerically using
+        // the sortInt() function above
+        var indexes = [];
+        for (index in table)
+            indexes.push (parseInt (index));
+        indexes.sort (sortInt);
+        
+        // Use the sorted indexes we've calculated to walk through each
+        // row in order
+        for (var i = 0; i < indexes.length; i++) {
+            // Like indexes we sort by column, so use the same trick here,
+            // some rows may not have the same columns as other rows, so
+            // we calculate this per row
+            var columns = [];
+            for (column in table[indexes[i]])
+                columns.push (parseInt (column));
+            columns.sort (sortInt);
+            
+            // Print index, then each column indented under the index
+            console.log ("row for index = " + indexes[i]);
+            for (var j = 0; j < columns.length; j++) {
+                console.log ("   column " + columns[j] + " = "
+                        + table[indexes[i]][columns[j]]);
+            }
+        }
+    }
+}
 //Funcion que recibe la respuesta get de snmp
 function callbackGet(error, varbinds){
     if (error) {
@@ -69,7 +109,10 @@ function getNext(mib, oid){
 }
 
 function getTable(mib, oid){
-    //TODO
+    if(mib!=4){
+        oid=mib[oid].oid;
+    }
+    session.table(oid,callbackTable);
 }
 
 //Funcion que espera al resultado snmp para devolver la respuesta
